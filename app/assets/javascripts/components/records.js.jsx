8d1fe3f4260f4e31a3524420eb76bf97d2f159e3
Andriy Ermoshin <lm73@rambler.ru>
2017-09-23 06:52:19 +0300

    Add amount box

--- app/assets/javascripts/components/records.js.jsx
+++ app/assets/javascripts/components/records.js.jsx
@@ -3,15 +3,44 @@ class Records extends React.Component{
     super(props);
     this.state = { records: props.records };
     this.addRecord = this.addRecord.bind(this);
+    this.credits = this.credits.bind(this);
+    this.debits = this.debits.bind(this);
+    this.balance = this.balance.bind(this);
   }
   addRecord(record){
     return(
       this.setState(prevState => { records: prevState.records.push(record)})
     )
   }
+  credits(){
+    return this.state.records
+      .filter(val => {
+        return val.amount >= 0;
+      })
+      .reduce((prev, curr) => {
+        return prev + parseFloat(curr.amount);
+    }, 0);
+  }
+  debits(){
+    return this.state.records
+      .filter(val =>{
+        return val.amount < 0;
+      })
+      .reduce((prev, curr) =>{
+        return prev + parseFloat(curr.amount);
+      }, 0);
+  }
+  balance(){
+    return this.debits() + this.credits();
+  }
   render(){
     return(
       <div className='records'>
+        <div className='row'>
+          <AmountBox type='success' amount={this.credits()} text='Credit'/>
+          <AmountBox type='danger' amount={this.debits()} text='Debit'/>
+          <AmountBox type='info' amount={this.balance()} text='Balance'/>
+        </div>
         <RecordForm addRecord={this.addRecord} />
         <hr />
         <table className='table table-bordered'>
