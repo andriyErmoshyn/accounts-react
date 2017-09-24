13e796584abbef2b6fb8e195b5569bffead7b433
Andriy Ermoshin <lm73@rambler.ru>
2017-09-24 09:25:45 +0300

    Move AmountBox to functional component

--- app/assets/javascripts/components/amount_box.js.jsx
+++ app/assets/javascripts/components/amount_box.js.jsx
@@ -1,12 +1,10 @@
-class AmountBox extends React.Component{
-  render(){
-    return(
-      <div className='col-md-4'>
-        <div className={"panel panel-" + this.props.type}>
-          <div className='panel-heading'>{this.props.text}</div>
-          <div className='panel-body'>{amountFormat(this.props.amount)}</div>
-        </div>
+const AmountBox = (props) =>{
+  return(
+    <div className='col-md-4'>
+      <div className={"panel panel-" + props.type}>
+        <div className='panel-heading'>{props.text}</div>
+        <div className='panel-body'>{amountFormat(props.amount)}</div>
       </div>
-    )
-  }
+    </div>
+  )
 }
